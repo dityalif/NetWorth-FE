91927f80ea17354da8cc095bbe4b11b2e1449d18
Raditya Alif <ditya818@gmail.com>
2025-05-01 23:37:04 +0700

    add skeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "@/lib/axios";
 import SearchBar from "@/components/SearchBar";
 import ProductCard from "@/components/Card";
+import SkeletonCard from "@/components/SkeletonCard"; 
 
 function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // Fetch data produk dari backend
   useEffect(() => {
     const getProducts = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(`/item`); // Mengambil data dari endpoint backend
+        const response = await axios.get(`/item`); 
         console.log("Products fetched:", response.data.payload);
-        setProducts(response.data.payload); // Menyimpan semua produk dari backend
+        setProducts(response.data.payload); 
       } catch (error) {
         console.error("Error fetching items:", error);
       } finally {
@@ -31,24 +31,25 @@ function Home() {
 
   return (
     <div className="relative flex flex-col min-h-screen w-full p-6">
-      {/* Nama website */}
       <h1 className="text-4xl font-extrabold text-center mb-4">NetWorth</h1>
-      {/* Search bar di tengah secara horizontal */}
       <div className="flex justify-center mb-6">
         <SearchBar placeholder="Explore more..." onSearch={handleSearch} />
       </div>
-      {/* Konten utama */}
       {loading ? (
-        <p className="text-center text-lg">Loading products...</p>
+        <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <SkeletonCard key={index} />
+          ))}
+        </main>
       ) : (
         <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {products.map((product) => (
             <ProductCard
               key={product.id}
-              image={product.image_url || "/store.svg"} // Gunakan gambar default jika tidak ada
+              image={product.image_url || "/store.svg"} 
               name={product.name}
               price={`Rp${product.price}`}
-              store={`Store ID: ${product.store_id}`} // Menampilkan ID toko
+              store={`Store ID: ${product.store_id}`} 
               stock={product.stock}
             />
           ))}
